a18b467dcb01fa8f9ea56609119c46766ede14ff
virginiamuleia <virginiamuleia794@gmail.com>
2024-08-08 15:44:56 +0200

    ja esta boss

--- Poo/src/poo/Poo.java
+++ Poo/src/poo/Poo.java
@@ -18,6 +18,7 @@ public class Poo {
    public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Que comece o trabalho semestral");
+        boolean a = if 
         //
     }
     
